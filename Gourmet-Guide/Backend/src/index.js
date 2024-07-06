317b0b554d853f7da3a7388f8decdfcc9a4defa4
prasun <prasun.1928@gmail.com>
2024-07-06 13:02:12 +0530

    testing

--- Gourmet-Guide/Backend/src/index.js
+++ Gourmet-Guide/Backend/src/index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import { userRouter } from './routes/users.js'
+ const app=express()
+ app.use(cors())
+ app.use(express.jason())
+ app.use('/auth',userRouter)
+ mongoose.connect("mongodb://localhost:27017/")
+ const port =5000
+ app.get('/',(req,res)=>{
+     res.send('hello world')
+ })
+ app.listen(port,()=>
+{
+    console.log('server is running')
+})
